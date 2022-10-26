ad4ab74466da249b3ed7d12a7c26c26e267bbb47
qwq <3208216735@qq.com>
2022-10-26 12:48:25 +0800

    增加了me命名空间的获取鼠标位置函数

---
+++ Project_release/Project/1.h
@@ -0,0 +1,6 @@
+#pragma once
+namespace me
+{
+	void Mouselo();//输出10次鼠标位置
+	void mouselo();//有判断的Mouselo()
+}
---
+++ Project_release/Project/源.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<Windows.h>
+#include"1.h"
+int main()
+{
+	std::cout << "hello" << std::endl;
+	me::mouselo();
+	return 0;
+}
